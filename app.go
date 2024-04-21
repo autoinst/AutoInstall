a5dd0c75fcca7548fae012b7c37b4c31c85803e2
jdnjk <2394058778@qq.com>
2024-04-21 20:22:43 +0800

    Update app.go

--- app.go
+++ app.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "os"
+    "path/filepath"
+    "strings"
+) //导入
 
 func main() {
     fmt.Println("[AutoInstall] 启动")
-
+    fmt.Println("查找pack.zip")
+    fileInfo, err := os.Stat("pack.zip")
+    if err != nil {
+        if os.IsNotExist(err) {
+            log.Fatal("无法找到文件")
+        }
+    }
 }
