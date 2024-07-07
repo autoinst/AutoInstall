52cfbf79ddec552768753c9be5df977e3483827f
jdnjk <2394058778@qq.com>
2024-07-07 22:06:24 +0800

    更新 app.go

--- app.go
+++ app.go
@@ -1,10 +1,16 @@
 package main
 
 import (
-    "fmt"
+    "fmt",
+    "os"
  )//导入
 
 func main() {
     fmt.Println("[AutoInstall] 启动")
-    fmt.Println("查找pack.zip")
+    fmt.Println("查找可执行文件中")
+    if _, err := os.Stat("fastinst.json"); err == nil {
+      fmt.Printf("诶wc\n");
+   } else {
+      fmt.Printf("找不到\n");
+   }
 }
