8b5d6903f8d7807e073d7c418612a92c0a64543c
jdnjk <2394058778@qq.com>
2024-07-08 20:55:42 +0800

    Update app.go

--- app.go
+++ app.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-    "fmt",
+    "fmt";
     "os"
  )//导入
 
 func main() {
     fmt.Println("[AutoInstall] 启动")
     fmt.Println("查找可执行文件中")
-    if _, err := os.Stat("fastinst.json"); err == nil {
+    if _, err := os.Stat("./fastinst.json"); err == nil {
       fmt.Printf("诶wc\n");
    } else {
       fmt.Printf("找不到\n");
