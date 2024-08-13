584705afb63ea658b49a70dfcc8a7117b0dc48ff
jdnjk <2394058778@qq.com>
2024-08-13 17:32:43 +0800

    修改

--- app.go
+++ app.go
@@ -2,15 +2,23 @@ package main
 
 import (
     "fmt";
-    "os"
+    "os";
+    "net/http"
  )//导入
 
 func main() {
+    aria2 := "https://www.twle.cn/static/i/img1.jpg"
     fmt.Println("[AutoInstall] 启动")
     fmt.Println("查找可执行文件中")
-    if _, err := os.Stat("./fastinst.json"); err == nil {
-      fmt.Printf("诶wc\n");
+    if _, err := os.Stat("./.authinst"); err == nil {
+      fmt.Printf("ok\n");
    } else {
-      fmt.Printf("找不到\n");
+      fmt.Printf("创建执行文件\n");
+      os.MkdirAll(".authinst/aria2", os.ModePerm)
+      resp, err := http.Get(aria2)
+      if err != nil {
+          return err
+      }
+      defer resp.Body.Close()
    }
 }
