e65de4f2e3ca2e16e016ba22c281fc3d5adbec9d
jdnjk <2394058778@qq.com>
2024-08-16 19:22:11 +0800

    Update app.go

--- app.go
+++ app.go
@@ -7,12 +7,24 @@ import (
  )//导入
 
 func main() {
-    aria2 := "https://www.twle.cn/static/i/img1.jpg"
-    fmt.Println("[AutoInstall] 启动")
-    fmt.Println("查找可执行文件中")
-    if _, err := os.Stat("./.authinst"); err == nil {
-      fmt.Printf("ok\n");
+   fmt.Println("[AutoInstall] 启动")
+   fmt.Println("查找可执行文件中")
+   if _, err := os.Stat("./.authinst"); err == nil {
+      fmt.Printf("200 OK\n");
    } else {
-      fmt.Printf("创建执行文件\n");
-      os.MkdirAll(".authinst/aria2", os.ModePerm)
+      os.MkdirAll(".authinst", os.ModePerm)
+   }
+   fmt.Printf("启动方式\n");
+   fmt.Printf("1.WEB操作(1)\n");
+   fmt.Printf("2.命令行启动(2)\n");
+   scanner.Scan()
+   start := scanner.Text()
+   if start = 0 {
+
+	} else if start = 1{
+
+	} else {
+      fmt.Printf("?你在干啥\n");
+      os.Exit(0)
+   }
 }
