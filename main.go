4cf38a92f3cbb521a93bdda8821f5fedec30469d
jdnjk <2394058778@qq.com>
2025-03-23 19:57:16 +0800

    .

--- main.go
+++ main.go
@@ -1,102 +1,120 @@
 package main
 
 import (
-	"bufio"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
+	"net/http"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"runtime"
 	"strings"
-	"time"
-) //导入
+)
 
-// 重置已有日志
-func renameFileToDateTime(oldFilePath string) (newFilePath string, err error) {
-	// 获取当前日期
-	date := time.Now().Format("2006-01-02")
+// InstConfig 结构体表示 inst.json 的内容
+type InstConfig struct {
+	Version       string `json:"version"`
+	Loader        string `json:"loader"`
+	LoaderVersion string `json:"loaderVersion"`
+	Download      string `json:"download"`
+}
 
-	// 构建新的文件名
-	newFileName := "Autoinst-" + date + ".txt"
-	newFilePath = filepath.Join(filepath.Dir(oldFilePath), newFileName)
-
-	// 检查文件是否存在，如果存在，则添加递增的数字
-	counter := 1
-	for {
-		if _, err := os.Stat(newFilePath); os.IsNotExist(err) {
-			break // 文件不存在，可以重命名
-		}
-		// 文件存在，添加数字后缀
-		newFileName = fmt.Sprintf("Autoinst-%s_%d.txt", date, counter)
-		newFilePath = filepath.Join(filepath.Dir(oldFilePath), newFileName)
-		counter++
-	}
-
-	// 重命名文件
-	err = os.Rename(oldFilePath, newFilePath)
+// 下载文件的函数
+func downloadFile(url, filePath string) error {
+	resp, err := http.Get(url)
 	if err != nil {
-		return "", err
+		return fmt.Errorf("无法下载文件: %v", err)
+	}
+	defer resp.Body.Close()
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		return fmt.Errorf("无法创建文件: %v", err)
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		return fmt.Errorf("无法写入文件: %v", err)
 	}
 
-	return newFilePath, nil
+	return nil
+}
+
+func findJava() (string, bool) {
+	javaHome := os.Getenv("JAVA_HOME")
+	if javaHome != "" {
+		javaPath := filepath.Join(javaHome, "bin", "java")
+		if _, err := os.Stat(javaPath); err == nil {
+			return javaPath, false
+		}
+	}
+
+	cmd := exec.Command("java", "-version")
+	output, err := cmd.CombinedOutput()
+	if err == nil && strings.Contains(string(output), "version") {
+		return "java", false
+	}
+
+	if runtime.GOOS == "linux" {
+		fallbackPath := "/usr/bin/jdk/jdk1.8.0_361/bin/java"
+		if _, err := os.Stat(fallbackPath); err == nil {
+			return fallbackPath, true
+		}
+	}
+
+	return "", false
 }
 
 func main() {
-	logFilePath := "./.autoinst/logs/laster.txt"
-
-	fmt.Println("AutoInstall初始化")
-	if _, err := os.Stat("./.autoinst"); err == nil {
-		fmt.Println("OK")
-	} else {
-		os.MkdirAll(".autoinst", os.ModePerm)
-	}
-
-	if _, err := os.Stat("./.autoinst/logs"); err == nil {
-		if _, err := os.Stat("./.autoinst/logs/laster.txt"); err == nil {
-			oldFilePath := "./.autoinst/logs/laster.txt"
-			// 调用函数重命名文件
-			newFilePath, err := renameFileToDateTime(oldFilePath)
-			if err != nil {
-				fmt.Println("无法重置日志:", err)
-				return
-			}
-			fmt.Println("日志重置完成:", newFilePath)
+	instFile := "inst.json"
+	var config InstConfig
+	if _, err := os.Stat(instFile); err == nil {
+		data, err := os.ReadFile(instFile)
+		if err != nil {
+			log.Println("无法读取 inst.json 文件:", err)
+			return
 		}
-	} else {
-		os.MkdirAll(".autoinst/logs", os.ModePerm)
-	}
-	logFile, err := os.Create(logFilePath)
 
-	// 设置日志输出到文件
-	multiWriter := io.MultiWriter(os.Stdout, logFile)
-	log.SetOutput(multiWriter)
+		if err := json.Unmarshal(data, &config); err != nil {
+			log.Println("无法解析 inst.json 文件:", err)
+			return
+		}
 
-	if _, err := os.Stat("./.autoinst/cache"); err == nil {
-		log.Println("已有缓存文件")
+		fmt.Println("准备安装:")
+		fmt.Printf("Minecraft版本: %s\n", config.Version)
+		fmt.Printf("加载器: %s\n", config.Loader)
+		fmt.Printf("加载器版本: %s\n", config.LoaderVersion)
+		fmt.Printf("下载源: %s\n", config.Download)
+
+		javaPath, simpfun := findJava()
+		if javaPath == "" {
+			log.Println("未找到 Java，请确保已安装 Java 并设置 JAVA_HOME。")
+			return
+		}
+		fmt.Println("找到 Java 运行环境:", javaPath)
+		if simpfun {
+			fmt.Println("使用备用 Java 位置，已启用simpfun特调")
+		}
+
+		if config.Loader == "neoforge" && config.Download == "bmclapi" {
+			installerURL := fmt.Sprintf(
+				"https://bmclapi2.bangbang93.com/maven/net/neoforged/neoforge/%s/neoforge-%s-installer.jar",
+				config.LoaderVersion, config.LoaderVersion,
+			)
+			installerPath := filepath.Join("./.autoinst/cache", fmt.Sprintf("neoforge-%s-installer.jar", config.LoaderVersion))
+			fmt.Println("检测到 neoforge 加载器，正在从 BMCLAPI 下载:", installerURL)
+			if err := downloadFile(installerURL, installerPath); err != nil {
+				log.Println("下载 neoforge 失败:", err)
+			} else {
+				fmt.Println("neoforge 安装器下载完成:", installerPath)
+			}
+		}
+	} else if os.IsNotExist(err) {
+		log.Println("inst.json 文件不存在")
 	} else {
-		os.MkdirAll(".autoinst/cache", os.ModePerm)
-	}
-	//开始安装
-	log.Printf("启动方式\n")
-	log.Printf("1.WEB操作(1)\n")
-	log.Printf("2.命令行启动(2)\n")
-	reader := bufio.NewReader(os.Stdin)
-	text, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println("读取输入时发生错误:", err)
-		return
-	}
-	text = strings.TrimSpace(text)
-	if text == "1" {
-		log.Printf("帮我写Vue?\n")
-		log.Printf("了解一下https://github.com/jdnjk/autoinst_web\n")
-		log.Printf("10秒后跳转到命令行\n")
-		time.Sleep(10 * time.Second)
-	} else if text == "2" {
-		log.Printf("启动命令行\n")
-		//cmd.InitBase()
-	} else {
-		log.Printf("?你在干啥\n")
-		os.Exit(0)
+		log.Println("无法访问 inst.json 文件:", err)
 	}
 }
