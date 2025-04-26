efd91f3b99343de395f8462679b87a4a3ffa8b7e
jdnjk <2394058778@qq.com>
2025-04-26 16:39:17 +0800

    6

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ set -e
 git_hash=$(git rev-parse --short HEAD 2>/dev/null)
 version="1.1.0-${git_hash}"
 echo "Build: AutoInstall-${version}"
-go build -ldflags "-X main.version=${version}" -o dist/${build_name} main.go
+go build -ldflags "-X main.version=${version}" -o dist/$BUILD_NAME main.go
 ls dist
