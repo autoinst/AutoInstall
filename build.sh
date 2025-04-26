f0991d17a517e4e18d8b9102bfd160941dcfffc7
jdnjk <2394058778@qq.com>
2025-04-26 16:37:05 +0800

    添加版本构建输出

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 set -e
 git_hash=$(git rev-parse --short HEAD 2>/dev/null)
 version="1.1.0-${git_hash}"
+echo "Build: AutoInstall-${version}"
 go build -ldflags "-X main.version=${version}" -o dist/${build_name} main.go
 ls dist
