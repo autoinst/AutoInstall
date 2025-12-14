59546ae220e7d9e22f7bb922b69810dc9c228939
jdnjk <2394058778@qq.com>
2025-12-14 10:08:43 +0800

    修复构建问题

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ set -e
 git_hash=$(git rev-parse --short HEAD 2>/dev/null)
 version="1.3.0-${git_hash}"
 echo "Build: AutoInstall-${version}"
-go build -ldflags "-X main.gitversion=${version} -X github.com/autoinst/AutoInstall/pkg.cfapiKey=${{ secrets.CF_API_KEY }}" -o dist/$BUILD_NAME main.go
+go build -ldflags "-X main.gitversion=${version} -X github.com/autoinst/AutoInstall/pkg.cfapiKey=${CF_API_KEY}" -o dist/$BUILD_NAME main.go
 ls dist
