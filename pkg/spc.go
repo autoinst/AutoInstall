e75b366db4dc316200bb76b22c7185e7f07a342d
zhazha7hao <88194458+zhazha7hao@users.noreply.github.com>
2026-01-20 19:22:11 +0800

    修复了因为variables.txt文件缺少内容导致的异常退出问题

--- pkg/spc.go
+++ pkg/spc.go
@@ -23,10 +23,23 @@ func SPCInstall(file string, MaxCon int, Args string) {
 		core.Log("读取 variables.txt 失败:", err)
 		return
 	}
+	version, okV := vars["MINECRAFT_VERSION"]
+	loader, okL := vars["MODLOADER"]
+	loaderVersion, okLV := vars["MODLOADER_VERSION"]
+
+	if !okV || version == "" || !okL || loader == "" || !okLV || loaderVersion == "" {
+		core.Log("错误：variables.txt 中缺少必要配置项 (MINECRAFT_VERSION, MODLOADER 或 MODLOADER_VERSION)")
+		core.Log("请检查您的 variables.txt 是否包含以下内容：")
+		core.Log("MINECRAFT_VERSION=1.20.1")
+		core.Log("MODLOADER=fabric")
+		core.Log("MODLOADER_VERSION=0.15.7")
+		return
+	}
+
 	instConfig := core.InstConfig{
-		Version:        vars["MINECRAFT_VERSION"],
-		Loader:         strings.ToLower(strings.ToLower(vars["MODLOADER"])),
-		LoaderVersion:  vars["MODLOADER_VERSION"],
+		Version:        version,
+		Loader:         strings.ToLower(loader),
+		LoaderVersion:  loaderVersion,
 		Download:       "bmclapi",
 		MaxConnections: 32,
 		Argsment:       "-Xmx{maxmen}M -Xms{maxmen}M -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -XX:G1MaxNewSizePercent=40 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -XX:MaxGCPauseMillis=200 -XX:MaxTenuringThreshold=1 -XX:SurvivorRatio=32 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true",
